8a412e4ad73834e80639bcfdb73739a30293fdad
Jmeow <j@jmeow.net>
2025-07-05 10:26:15 -0500

    Use different API for getting messages

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
-import { LogLevel, ReactionAddedEvent, WebClient } from "npm:@slack/web-api"
-import { SocketModeClient } from "npm:@slack/socket-mode"
+import { LogLevel, WebClient } from "npm:@slack/web-api"
 import "jsr:@std/dotenv/load"
 import { createCanvas } from "https://deno.land/x/canvas/mod.ts"
 
@@ -40,61 +39,6 @@ const client = new WebClient(Deno.env.get("OAUTH_TOKEN"), {
 
 const channelId = Deno.env.get("SLACK_CHANNEL_ID")!
 
-const socketClient = new SocketModeClient({ appToken: Deno.env.get("SOCKET_TOKEN")!, logLevel: LogLevel.INFO })
-
-const messages: { [key: string]: string } = {}
-
-socketClient.on("reaction_added", async (ev) => {
-    const event = ev.event as ReactionAddedEvent
-    // console.log(event)
-    if (ev.retry_num) {
-        return
-    }
-    if (event.item.type == "message") {
-        if (event.item.channel == channelId) {
-            if (event.user == Deno.env.get("RTM_BOT_ID")!) {
-                // deno-lint-ignore no-inner-declarations
-                async function handle() {
-                    const ts = event.item.ts
-                    const text = messages[ts]!
-                    if (event.reaction == "white_check_mark") {
-                        if (text && !isNaN(parseInt(text)) && Math.abs(parseInt(text)) < 100) {
-                            data.push(parseInt(text))
-                            console.log(parseInt(text))
-                            delete messages[ts]
-                        } else {
-                            console.log(text, !isNaN(parseInt(text)), Math.abs(parseInt(text)))
-                        }
-                    }
-                    if (event.reaction == "tada" && data.length > 0) {
-                        console.log("Game end")
-                        data.push(parseInt(text))
-                        await sendGraph(getGraph(data))
-                        data = []
-                        console.log("Game end message sent")
-                    }
-                }
-                if (event.item.ts in messages) {
-                    handle()
-                } else {
-                    setTimeout(handle, 5000)
-                }
-            }
-        }
-    }
-})
-socketClient.on("message", (ev) => {
-    if (ev.retry_num) return
-    const event = ev.event
-    messages[event.ts] = event.text
-    setTimeout(() => {
-        if (event.ts in messages) {
-            delete messages[event.ts]
-        }
-    }, 60000)
-})
-await socketClient.start()
-
 client.chat.postEphemeral({
     user: Deno.env.get("ALERT_USER_ID")!,
     attachments: [],
@@ -131,9 +75,35 @@ async function sendGraph(buffer: Uint8Array) {
         console.error(completeUploadResponse.error)
     }
 }
-// await sendGraph(
-//     getGraph([
-//         0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1, 0, -1, -2, -1, -2, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83,
-//         84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 94, 95, 96, 97, 98, 99, 100,
-//     ])
-// )
+function startSocket() {
+    const socket = new WebSocket(Deno.env.get("EVENTS_WS_URL")!)
+
+    socket.addEventListener("open", () => {
+        console.log(socket.readyState)
+    })
+
+    socket.addEventListener("message", async (event) => {
+        console.log(event.data)
+        const json = JSON.parse(event.data)
+        if (json.type == "count" || json.type == "screwed-up") {
+            data.push(json.resultingCount)
+        }
+        if (json.type == "win") {
+            await sendGraph(getGraph(data))
+            data = []
+        }
+    })
+    const interval = setInterval(() => {
+        socket.send(new Uint8Array([0]))
+    }, 10000)
+
+    socket.addEventListener("close", () => {
+        clearInterval(interval)
+        startSocket()
+    })
+    socket.addEventListener("error", () => {
+        clearInterval(interval)
+        startSocket()
+    })
+}
+startSocket()
